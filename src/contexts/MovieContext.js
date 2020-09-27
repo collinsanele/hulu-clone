4961f9317b849277ef3138d64722b2a926928d28
Collins Anele <45266007+collinsanele@users.noreply.github.com>
2020-09-27 10:47:47 +0100

    Update MovieContext.js

--- src/contexts/MovieContext.js
+++ src/contexts/MovieContext.js
@@ -15,7 +15,7 @@ class MovieContextProvider extends React.Component{
     
     //Get data for Trending movies
     fetchTrending = ()=>{
-        fetch("https://api.themoviedb.org/3/trending/all/week?api_key=9055835fc1742faabb67105c2c172af3&language=en-US")
+        fetch("https://api.themoviedb.org/3/trending/all/week?api_key=Key&language=en-US")
         .then((response)=> response.json())
         .then((data)=>this.setState({movies: data.results.splice(0, 20)}))
     }
@@ -24,7 +24,7 @@ class MovieContextProvider extends React.Component{
     
     //Get data for Top Rated movies
     fetchTopRated = ()=>{
-        fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=9055835fc1742faabb67105c2c172af3&language=en-US")
+        fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=key&language=en-US")
         .then((response)=> response.json())
         .then((data)=>this.setState({movies: data.results.length > 20 ? data.results.splice(0, 20) : data.results}))
     }
@@ -33,7 +33,7 @@ class MovieContextProvider extends React.Component{
     
     //Get data for Action movies
     fetchAction = ()=>{
-        fetch("https://api.themoviedb.org/3/discover/movie?api_key=9055835fc1742faabb67105c2c172af3&with_genres=28")
+        fetch("https://api.themoviedb.org/3/discover/movie?api_key=key&with_genres=28")
         .then((response)=> response.json())
         .then((data)=>this.setState({movies: data.results.length > 20 ? data.results.splice(0, 20) : data.results}))
     }
@@ -41,7 +41,7 @@ class MovieContextProvider extends React.Component{
     
     //Get data for Horror movies
     fetchHorror = ()=>{
-        fetch("https://api.themoviedb.org/3/discover/movie?api_key=9055835fc1742faabb67105c2c172af3&with_genres=27")
+        fetch("https://api.themoviedb.org/3/discover/movie?api_key=key&with_genres=27")
         .then((response)=> response.json())
         .then((data)=>this.setState({movies: data.results.length > 20 ? data.results.splice(0, 20) : data.results}))
     }
@@ -49,7 +49,7 @@ class MovieContextProvider extends React.Component{
     
     //Get data for Romannce movies
     fetchRomance = ()=>{
-        fetch("https://api.themoviedb.org/3/discover/movie?api_key=9055835fc1742faabb67105c2c172af3&with_genres=10749")
+        fetch("https://api.themoviedb.org/3/discover/movie?api_key=key&with_genres=10749")
         .then((response)=> response.json())
         .then((data)=>this.setState({movies: data.results.length > 20 ? data.results.splice(0, 20) : data.results}))
     }
@@ -58,7 +58,7 @@ class MovieContextProvider extends React.Component{
     
     //Get data for mystery movies
     fetchMystery = ()=>{
-        fetch("https://api.themoviedb.org/3/discover/movie?api_key=9055835fc1742faabb67105c2c172af3&with_genres=9648")
+        fetch("https://api.themoviedb.org/3/discover/movie?api_key=key&with_genres=9648")
         .then((response)=> response.json())
         .then((data)=>this.setState({movies: data.results.length > 20 ? data.results.splice(0, 20) : data.results}))
     }
@@ -66,7 +66,7 @@ class MovieContextProvider extends React.Component{
     
     //Get data for sci-fi movies
     fetchSciFi = ()=>{
-        fetch("https://api.themoviedb.org/3/discover/movie?api_key=9055835fc1742faabb67105c2c172af3&with_genres=878")
+        fetch("https://api.themoviedb.org/3/discover/movie?api_key=key&with_genres=878")
         .then((response)=> response.json())
         .then((data)=>this.setState({movies: data.results.length > 20 ? data.results.splice(0, 20) : data.results}))
     }
@@ -75,7 +75,7 @@ class MovieContextProvider extends React.Component{
     
     //Get data for Western movies
     fetchWestern = ()=>{
-        fetch("https://api.themoviedb.org/3/discover/movie?api_key=9055835fc1742faabb67105c2c172af3&with_genres=37")
+        fetch("https://api.themoviedb.org/3/discover/movie?api_key=key&with_genres=37")
         .then((response)=> response.json())
         .then((data)=>this.setState({movies: data.results.length > 20 ? data.results.splice(0, 20) : data.results}))
     }
@@ -83,7 +83,7 @@ class MovieContextProvider extends React.Component{
     
     //Get data for Animation movies
     fetchAnimation = ()=>{
-        fetch("https://api.themoviedb.org/3/discover/movie?api_key=9055835fc1742faabb67105c2c172af3&with_genres=16")
+        fetch("https://api.themoviedb.org/3/discover/movie?api_key=key&with_genres=16")
         .then((response)=> response.json())
         .then((data)=>this.setState({movies: data.results.length > 200 ? data.results.splice(0, 200) : data.results}))
     }
